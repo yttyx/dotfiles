d1aadc07fec3e6ef87b2f6a333dd3f3a3bb37bc4
yttyx <yttyx@picasa.org.uk>
2022-03-13 12:02:45 +0000

    Some LSP functionality now working in C++

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,14 @@
+--[[ This configuration is heavily based on chris@machine's dotfiles at:
+     https://github.com/LunarVim/Neovim-from-scratch
+
+     Status:
+       - C++ error detection is starting to work
+       - no code completion yet
+       - no goto definition/declaration etc. yet
+       - install nerdtree font for completion icons?
+--]]
+
+
 require "user.options"
 require "user.keymaps"
 require "user.plugins"
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -40,10 +40,25 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
+  -- General
   use "wbthomason/packer.nvim"          -- Have packer manage itself
   use "nvim-lua/popup.nvim"             -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"           -- Useful lua functions used by lots of plugins
+  use "windwp/nvim-autopairs"           -- Autopairs, integrates with both cmp and treesitter
+  use "numToStr/Comment.nvim"           -- Easily comment stuff
+  use "kyazdani42/nvim-web-devicons"
+  use "kyazdani42/nvim-tree.lua"
+  use "akinsho/bufferline.nvim"
+  use "moll/vim-bbye"
+  use "nvim-lualine/lualine.nvim"
+  use "akinsho/toggleterm.nvim"
+  use "ahmedkhalf/project.nvim"
+  use "lewis6991/impatient.nvim"
+  use "lukas-reineke/indent-blankline.nvim"
+  use "goolord/alpha-nvim"
+  use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
+  use "folke/which-key.nvim"
+
   use "gruvbox-community/gruvbox"
   use "tpope/vim-fugitive"
   use "tpope/vim-commentary"
@@ -62,9 +77,17 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip"        -- Snippet completions
   use "hrsh7th/cmp-nvim-lsp"
 
+  -- Snippets
+  use "L3MON4D3/LuaSnip"                -- snippet engine
+  use "rafamadriz/friendly-snippets"    -- a bunch of snippets to use
+
   -- LSP TBW
   use 'neovim/nvim-lspconfig'           -- collection of configurations for the built-in LSP client
   use "williamboman/nvim-lsp-installer" -- package manager
+
+  use "tamago324/nlsp-settings.nvim"    -- language server settings defined in json for
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+
   use 'nvim-lua/lsp-status.nvim'
   use 'nvim-lua/completion-nvim'
   use 'nvim-lua/diagnostic-nvim'
